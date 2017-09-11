4cd5b2c5813f8c051c4dbf7971a8e60d36bc30a8
Anthony Woodcock <anthonywoodcockmusic1991@hotmail.co.uk>
2017-09-11 19:55:17 +0100

    added a recipe list stateless component

--- src/Recipes.jsx
+++ src/Recipes.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import './Recipes.css'
+import RecipesList from './components/RecipeList'
+import superagent from 'superagent'
 
 class Recipes extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      recipes: []
+    }
+  }
+
+  componentDidMount () {
+    superagent.request.get('http://localhost:3001/recipes').end((error,response) =>{ 
+      if (!error) {
+        this.setState({ recipes: response.body })
+      }
+    })
+  }
+
   render(){
     return(
-      <div className="recipes-container" />
+      <div className="recipes-container">
+        <RecipesList recipes={this.state.recipes} />
+      </div>
     )
   }
 }
